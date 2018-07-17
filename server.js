c7463a1ff512e9d6dbd073a299a07228da149101
Dan Barthell <dbarthell@er-10-129-27-91.wireless.umn.edu>
2018-07-16 20:59:34 -0500

    remove break

--- server.js
+++ server.js
@@ -68,7 +68,6 @@ app.get("/scrape", function (req, res) {
                 .catch(function (err) {
                     // If an error occurred, send it to the client
                     return res.json(err);
-                    break;
                 });
 
         });
