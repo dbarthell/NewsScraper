eb312fa27cfa1e69bdfc5d10ba23a8e4027ab11e
Dan Barthell <dbarthell@Dans-MacBook-Pro.local>
2018-07-16 23:22:10 -0500

    put return back

--- server.js
+++ server.js
@@ -68,8 +68,7 @@ app.get("/scrape", function (req, res) {
                 })
                 .catch(function (err) {
                     // If an error occurred, send it to the client
-                    res.json(err);
-                    return;
+                    return res.json(err);
                 });
 
         });
