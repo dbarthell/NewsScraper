2ed2872ee568b689a1ca345a36419e6a5a3cb455
Dan Barthell <dbarthell@Dans-MacBook-Pro.local>
2018-07-19 21:12:35 -0500

    temp

--- models/Article.js
+++ models/Article.js
@@ -9,23 +9,23 @@ var ArticleSchema = new Schema({
   // `title` is required and of type String
   title: {
     type: String,
-    required: true
+    // required: true
   },
   // `summary` is required and of type String
   summary: {
       type: String,
-      required: true
+      // required: true
   },
 
   // `author` is of type String
   author: {
     type: String,
-    required: false
+    // required: false
   },
   // `link` is required and of type String
   link: {
     type: String,
-    required: true
+    // required: true
   },
   // `note` is an object that stores a Note id
   // The ref property links the ObjectId to the Note model
